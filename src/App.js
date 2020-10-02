07492608e996720e013220e6d5f01bd12ab18bc9
Satyam <satyambhardawaj@gmail.com>
2020-10-02 12:43:49 +0530

    authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+    }
+
+    console.log("The token is ", token);
+  }, [token]);
+
   return (
     <div className="App">
-      {/* Spotify Logo */}
-      {/* Login with spotify button */}
-      <Login />
+      {
+        token? (
+          <h1>I'm Logged in</h1>
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
