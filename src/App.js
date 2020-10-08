59f2712d33655033b78de64fa038f0f9527688de
Satyam <satyambhardawaj@gmail.com>
2020-10-08 16:10:16 +0530

    sidebar functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './Player';
+import { useStateProviderValue } from './StateProvider';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const[{user, token}, dispatch] = useStateProviderValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -12,17 +17,30 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
+      })
     }
 
     console.log("The token is ", token);
   }, [token]);
 
+
   return (
     <div className="App">
       {
         token? (
-          <h1>I'm Logged in</h1>
+          <Player spotify={spotify} />
         ) : (
           <Login />
         )
